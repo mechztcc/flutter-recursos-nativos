84f797bcec394974259f27c8a9e1f0a0a95ce018
mechztcc <dojathan@gmail.com>
2022-01-04 17:46:02 -0300

    add provider to Place

---
+++ lib/providers/great_places.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/widgets.dart';
+import 'package:recursos_nativos/models/place.dart';
+
+class GreatPlaces with ChangeNotifier {
+  List<Place> _items = [];
+
+  List<Place> get items {
+    return [..._items];
+  }
+
+  int get itemsCount {
+    return _items.length;
+  }
+
+  Place getItemByIndex(int index) {
+    return _items[index];
+  }
+}
